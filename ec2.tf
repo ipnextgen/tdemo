218579bcb63cd82d72e4198d56d33795385c1556
mglap <mglap@amazon.com>
2023-03-14 21:08:19 -0400

    instance type

--- ec2.tf
+++ ec2.tf
@@ -2,9 +2,10 @@
 
 resource "aws_instance" "web" {
   ami           = "ami-0e9d8c96b045dbc75"
-  instance_type = "t3.micro"
+  instance_type = var.instance
 
   tags = {
     Name = "HelloWorld"
+    Proprio = var.proprio
   }
 }
